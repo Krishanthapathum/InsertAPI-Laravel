917dba372b33cb604b123b71fafa62f78bebf12d
Krishanthapathum <kgkppremalal@students.nsbm.ac.lk>
2025-03-26 14:53:38 +0530

    permit controller create

--- app/Http/Controllers/PermitUserController.php
+++ app/Http/Controllers/PermitUserController.php
@@ -10,26 +10,31 @@ class PermitUserController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'surname' => 'required|string',
-            'other_names' => 'required|string',
-            'date_of_birth' => 'required|date',
-            'sl_license_number' => 'required|string',
-            'international_permit_number' => 'required|string',
-            'issued_date' => 'required|date',
-            'expiry_date' => 'required|date',
-            'valid_vehicles' => 'required|string',
+            'last_name' => 'required|string',
+            'first_names' => 'required|string',
+            'dob' => 'required|date',
+            'sl_license_no' => 'nullable|string',
+            'int_permit_no' => 'nullable|string',
+            'date_issued' => 'required|date',
+            'date_expiry' => 'required|date',
+            'vehicle_types' => 'required|string',
         ]);
 
+        // Determine which identifier to use
+        $identifierField = $request->filled('int_permit_no') ? 'int_permit_no' : 'sl_license_no';
+        $identifierValue = $request->{$identifierField};
+
         $user = PermitUser::firstOrCreate(
-            ['international_permit_number' => $request->international_permit_number],
+            [$identifierField => $identifierValue],
             [
-                'surname' => $request->surname,
-                'other_names' => $request->other_names,
-                'date_of_birth' => $request->date_of_birth,
-                'sl_license_number' => $request->sl_license_number,
-                'issued_date' => $request->issued_date,
-                'expiry_date' => $request->expiry_date,
-                'valid_vehicles' => $request->valid_vehicles,
+                'last_name' => $request->last_name,
+                'first_names' => $request->first_names,
+                'dob' => $request->dob,
+                'sl_license_no' => $request->sl_license_no,
+                'int_permit_no' => $request->int_permit_no,
+                'date_issued' => $request->date_issued,
+                'date_expiry' => $request->date_expiry,
+                'vehicle_types' => $request->vehicle_types,
             ]
         );
 
@@ -38,4 +43,20 @@ class PermitUserController extends Controller
             'data' => $user
         ]);
     }
+
+    public function findByPermit($int_permit_no)
+    {
+        $int_permit_no = trim($int_permit_no);
+
+        $user = PermitUser::where('int_permit_no', $int_permit_no)->first();
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+
+        return response()->json(['data' => $user]);
+    }
+
+
+
 }
