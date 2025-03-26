7845612c1d79e0ee781a4e95101659d49ff39f68
Krishanthapathum <kgkppremalal@students.nsbm.ac.lk>
2025-03-26 16:11:07 +0530

    add route

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,11 @@ Route::get('/dashboard', function () {
 Route::get('/qr', [QRCodeController::class, 'index'])->name('qr.index');
 Route::post('/qr/generate', [QRCodeController::class, 'generate'])->name('qr.generate');
 
-// web.php
 Route::get('/user/{identifier}', [QRCodeController::class, 'showUserByQr'])->name('qr.show');
 
+Route::get('/generated-qrs', function () {
+    $users = \App\Models\PermitUser::whereNotNull('qr_code_identifier')->get();
+    return view('qr.generated', compact('users'));
+})->name('qr.generated.view');
+
+Route::get('/qr/export/pdf', [QRCodeController::class, 'exportPDF'])->name('qr.export.pdf');
